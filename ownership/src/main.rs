4b73e0b76b7ce5fd3657ff5018db6bc076916a3e
harumaki1220 <haruma181220@gmail.com>
2025-11-14 02:05:14 +0900

    ownershipプロジェクトを更新（参照と借用の学習）

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -12,9 +12,7 @@ fn main() {
     let s2 = s1; // s1からs2へ所有権が「ムーブ」する
 
     println!("s2 = {}", s2);
-    // 
-    // 
-    // 
+ 
 
     println!("--------------------------");
     println!("--- Vec (Move) の場合 ---");
@@ -22,15 +20,24 @@ fn main() {
     let v2 = v1; // v1からv2へ所有権が「ムーブ」する
 
     println!("v2 = {:?}", v2); // Vecの表示は :? が便利
-    // 
-    // 
+
 
     println!("--------------------------");
     println!("--- Box (Move) の場合 ---");
     let b1 = Box::new(5); // b1がBoxの所有権を持つ
     let b2 = b1; // b1からb2へ所有権が「ムーブ」する
-
     println!("b2 = {}", b2);
-    // 
-    // 
+
+    
+    println!("--------------------------");
+    println!("--- 関数とムーブ ---");
+    let s_main = String::from("this is a test");
+    let len = calculate_length(&s_main);
+
+    println!("'{}' の長さは {} です。", s_main, len);
+}
+
+fn calculate_length(s: &String) -> usize { // &String は「String 型への参照」
+    let length = s.len(); // .len() は文字列のバイト数を返す
+    length
 }
