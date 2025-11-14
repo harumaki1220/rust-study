fdb56e18c09ac54501392c83d88236d8a3710cc7
harumaki1220 <haruma181220@gmail.com>
2025-11-14 13:55:46 +0900

    ownershipプロジェクトを更新（可変参照 &mut の学習）

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -2,42 +2,52 @@ fn main() {
     println!("--- i32 (Copy) の場合 ---");
     let x = 5; // i32は「Copy」トレイトを持つ
     let y = x; // 値がコピーされる（ムーブではない）
-
-    // xもyも両方使える
-    println!("x = {}, y = {}", x, y);
+    println!("x = {}, y = {}", x, y); // xもyも両方使える
 
     println!("--------------------------");
     println!("--- String (Move) の場合 ---");
     let s1 = String::from("hello"); // s1がデータの所有権を持つ
     let s2 = s1; // s1からs2へ所有権が「ムーブ」する
-
     println!("s2 = {}", s2);
- 
+    // 
 
     println!("--------------------------");
     println!("--- Vec (Move) の場合 ---");
     let v1 = vec![1, 2, 3]; // v1がベクタの所有権を持つ
     let v2 = v1; // v1からv2へ所有権が「ムーブ」する
-
     println!("v2 = {:?}", v2); // Vecの表示は :? が便利
-
+    // 
 
     println!("--------------------------");
     println!("--- Box (Move) の場合 ---");
     let b1 = Box::new(5); // b1がBoxの所有権を持つ
     let b2 = b1; // b1からb2へ所有権が「ムーブ」する
     println!("b2 = {}", b2);
+    // 
 
-    
     println!("--------------------------");
-    println!("--- 関数とムーブ ---");
-    let s_main = String::from("this is a test");
-    let len = calculate_length(&s_main);
+    println!("--- 関数と借用 (&) ---");
+    let s_len = String::from("this is a test");
+    // 
+    let len = calculate_length(&s_len);
+    // 
+    println!("'{}' の長さは {} です。", s_len, len);
 
-    println!("'{}' の長さは {} です。", s_main, len);
+    println!("--------------------------");
+    println!("--- 関数と可変参照 (&mut) ---");
+    let mut s_mut = String::from("hello"); // 
+    change_string(&mut s_mut); // 
+    println!("変更後のs_mut: {}", s_mut); // 
 }
 
+// 
+// 
 fn calculate_length(s: &String) -> usize { // &String は「String 型への参照」
-    let length = s.len(); // .len() は文字列のバイト数を返す
-    length
+    s.len()
+}
+
+// 
+// 
+fn change_string(s: &mut String) {
+    s.push_str(" world"); // .push_str() は文字列に文字を追加する
 }
